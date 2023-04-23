c2cee486ec45ad257d6cc29fb3896873e5b468e3
sumutoni <54947406+sumutoni@users.noreply.github.com>
2023-04-23 19:23:52 +0200

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,18 @@
 # configure ssh using puppet
 include stdlib
 
+file_line { 'Host':
+  ensure => 'present',
+  path   => '/root/.ssh/config',
+  line   => 'Host *'
+}
 file_line { 'password auth':
   ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '\tPasswordAuthentication no'
+  path   => '/root/.ssh/config',
+  line   => '   PasswordAuthentication no'
 }
 file_line { 'private key file':
   ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '\tIdentityFile ~/.ssh/school'
+  path   => '/root/.ssh/config',
+  line   => '   IdentityFile ~/.ssh/school'
 }
