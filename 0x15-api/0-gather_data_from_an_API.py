a377f9e6f46ecbc194fb0796e66b31764277278b
sumutoni <steumutoni@gmail.com>
2023-05-22 13:51:29 -0700

    using REST API to interact with external data

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Using a REST API to get todo list of a user based on id"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get('https://jsonplaceholder.typicode.com/todos')
+    u = requests.get('https://jsonplaceholder.typicode.com/users')
+    todo_list = []
+    completed = 0
+    completed_list = []
+    name = ''
+    for i in range(0, len(r.json())):
+        if r.json()[i]['userId'] == int(argv[1]):
+            todo_list.append(r.json()[i])
+    for i in range(0, len(todo_list)):
+        if todo_list[i]['completed']:
+            completed_list.append(todo_list[i]['title'])
+            completed += 1
+    for i in range(0, len(u.json())):
+        if u.json()[i]['id'] == int(argv[1]):
+            name = u.json()[i]['name']
+            break
+    print('Employee {} is done with tasks ({:d}/20):'.format(name, completed))
+    for i in range(0, len(completed_list)):
+        print('\t {}'.format(completed_list[i]))
