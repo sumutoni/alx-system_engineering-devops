f6e6d792a7285e630c9513c6b0c8c1b2ab6d3c90
sumutoni <steumutoni@gmail.com>
2023-06-06 14:23:19 -0700

    function that returns top 10 hot posts of subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" function that queries the Reddit API and prints the
+titles of the first 10 hot posts listed for a given subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    """return top 10 posts of subreddit"""
+    url = "https://www.reddit.com/r/{}.json".format(subreddit)
+    h = requests.utils.default_headers()
+    headers = {'User-Agent': "Ubuntu"}
+    h.update(headers)
+    r = requests.get(url, headers=h, allow_redirects=False).json()
+    posts = r.get('data', {}).get('children', [])
+    if not posts:
+        return None
+    else:
+        for post in posts:
+            print(post['data']['title'])
